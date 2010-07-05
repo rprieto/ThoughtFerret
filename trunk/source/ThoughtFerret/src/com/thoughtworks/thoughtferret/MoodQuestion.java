e5210a517aae07fe6182d3b82aa1bb6c93416801
rprieto <rprieto@578f6600-eed7-407d-a15d-0bb1fb5aace9>
2010-07-05 12:47:47 +0000

    Initial content, generated projects
    git-svn-id: https://subversion.assembla.com/svn/ThoughtFerret@2 578f6600-eed7-407d-a15d-0bb1fb5aace9

---
+++ trunk/source/ThoughtFerret/src/com/thoughtworks/thoughtferret/MoodQuestion.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.thoughtferret;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MoodQuestion extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
