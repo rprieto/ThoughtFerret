2e700c737a7c65ab39355d1d492843e3c15bdb95
rprieto <rprieto@578f6600-eed7-407d-a15d-0bb1fb5aace9>
2010-07-06 21:57:35 +0000

    Added empty words page, and cleaned up old files
    git-svn-id: https://subversion.assembla.com/svn/ThoughtFerret@8 578f6600-eed7-407d-a15d-0bb1fb5aace9

--- trunk/source/ThoughtFerret/src/com/thoughtworks/thoughtferret/HappyWords.java
+++ trunk/source/ThoughtFerret/src/com/thoughtworks/thoughtferret/HappyWords.java
@@ -3,12 +3,12 @@ package com.thoughtworks.thoughtferret;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MoodQuestion extends Activity {
-    
-    @Override
+public class HappyWords extends Activity {
+
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
-    
+        setContentView(R.layout.happywords);
+	}
+	
 }
