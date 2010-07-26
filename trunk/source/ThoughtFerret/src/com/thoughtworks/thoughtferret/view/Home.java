64502727bb90a075610d082f7790aaf9cae570b6
rprieto <rprieto@578f6600-eed7-407d-a15d-0bb1fb5aace9>
2010-07-26 15:05:42 +0000

    Added one random falling star on home screen
    git-svn-id: https://subversion.assembla.com/svn/ThoughtFerret@71 578f6600-eed7-407d-a15d-0bb1fb5aace9

--- trunk/source/ThoughtFerret/src/com/thoughtworks/thoughtferret/view/Home.java
+++ trunk/source/ThoughtFerret/src/com/thoughtworks/thoughtferret/view/Home.java
@@ -13,9 +13,18 @@ import android.graphics.Rect;
 import android.graphics.PorterDuff.Mode;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Display;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+import android.view.animation.TranslateAnimation;
+import android.view.animation.Animation.AnimationListener;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.thoughtworks.thoughtferret.R;
@@ -23,14 +32,21 @@ import com.thoughtworks.thoughtferret.view.moodgraph.MoodGraph;
 
 public class Home extends Activity {
 	
+	LinearLayout homeBackground;
+	ImageView starImage;
+	Rect screen;
+	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
 
-        BitmapDrawable drawable = new BitmapDrawable(createBackground());        
-        LinearLayout homeBackground = (LinearLayout) findViewById(R.id.homeBackground);
-        homeBackground.setBackgroundDrawable(drawable);
+        homeBackground = (LinearLayout) findViewById(R.id.homeBackground);
+        starImage = (ImageView) findViewById(R.id.homestar);
+
+        calculateScreen();
+        setBackground();
+        createAnimation();
         
 //        Typeface typeFace = Typeface.createFromAsset(getAssets(), "fonts/handmadetypewriter.ttf");
 //        TextView homeUpdate = (TextView) findViewById(R.id.homeUpdate);
@@ -60,10 +76,13 @@ public class Home extends Activity {
 		startActivity(new Intent(this, DemoHacks.class));
 	}
 	
-	public Bitmap createBackground() {
+	private void calculateScreen() {
 		Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
-        Rect screen = new Rect(0, 0, display.getWidth(), display.getHeight());        
-        ApplicationBackground appBackground = new ApplicationBackground(getResources(), screen.width(), screen.height(), ApplicationBackground.GradientDirection.HORIZONTAL, true);
+        screen = new Rect(0, 0, display.getWidth(), display.getHeight());
+	}
+	
+	private void setBackground() {
+		ApplicationBackground appBackground = new ApplicationBackground(getResources(), screen.width(), screen.height(), ApplicationBackground.GradientDirection.HORIZONTAL, true);
         Bitmap ferretBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.homeferret);       
         Bitmap smallFerret = Bitmap.createScaledBitmap(ferretBitmap, 200, 200, true);
         
@@ -74,13 +93,46 @@ public class Home extends Activity {
         Paint paint = new Paint(); 
         paint.setXfermode(new PorterDuffXfermode(Mode.MULTIPLY)); 
 
-        int left = (display.getWidth() - smallFerret.getWidth()) / 2;
-        int top = (display.getHeight() - smallFerret.getHeight()) / 2;
+        int left = (screen.width() - smallFerret.getWidth()) / 2;
+        int top = (screen.height() - smallFerret.getHeight()) / 2;
         Point topLeft = new Point(left, top);
         canvas.drawBitmap(smallFerret, topLeft.x, topLeft.y, paint);
         
-        return fullBackground;
+        BitmapDrawable drawable = new BitmapDrawable(fullBackground);        
+        homeBackground.setBackgroundDrawable(drawable);
 	}
 	
+	public void createAnimation() {
+		int x = (int) (Math.random() * screen.width());
+		TranslateAnimation fallingStar = new TranslateAnimation(x, x, 0, screen.bottom);
+		fallingStar.setStartOffset(0);
+		fallingStar.setDuration(5000);
+		fallingStar.setFillAfter(true);
+		fallingStar.setAnimationListener(animListener);		
+		starImage.startAnimation(fallingStar);
+	}
 	
+	private AnimationListener animListener = new LocalAnimationListener();
+	
+	class LocalAnimationListener implements AnimationListener
+    {
+        public void onAnimationEnd(Animation animation)
+        {
+            Handler curHandler = new Handler();
+            curHandler.postDelayed( new Runnable()
+            {
+                public void run()
+                {
+                	createAnimation();
+                }
+            }, 1);
+        }
+        public void onAnimationRepeat(Animation animation)
+        {
+        }
+        public void onAnimationStart(Animation animation)
+        {
+        }
+    };
+
 }
