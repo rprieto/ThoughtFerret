72b548fa962622badfa130b1ff7c72035acd528e
rprieto <rprieto@578f6600-eed7-407d-a15d-0bb1fb5aace9>
2010-08-07 14:44:17 +0000

    Fixed bugs in rating average calculation
    git-svn-id: https://subversion.assembla.com/svn/ThoughtFerret@89 578f6600-eed7-407d-a15d-0bb1fb5aace9

--- trunk/source/ThoughtFerret/src/com/thoughtworks/thoughtferret/view/moodgraph/RatingAverages.java
+++ trunk/source/ThoughtFerret/src/com/thoughtworks/thoughtferret/view/moodgraph/RatingAverages.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.thoughtferret.view.moodgraph;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.joda.time.DateTime;
@@ -12,16 +13,17 @@ public class RatingAverages {
 	List<RatingPeriod> periods;
 
 	public RatingAverages(MoodRatings moodRatings, int nbDaysInPeriod) {
+		periods = new ArrayList<RatingPeriod>();
 		calculatePeriods(moodRatings, nbDaysInPeriod);
 		calculateAverages(moodRatings);
 	}
 	
 	private void calculatePeriods(MoodRatings moodRatings, int nbDaysInPeriod) {
 		DateTime startDate = moodRatings.getFirst().getLoggedDate();
-		DateTime endDate = moodRatings.getFirst().getLoggedDate();
+		DateTime endDate = moodRatings.getLast().getLoggedDate();
 		
-		DateTime startMonth = startDate.withDayOfMonth(0);
-		DateTime endMonth = endDate.plusMonths(1).withDayOfMonth(0);
+		DateTime startMonth = startDate.withDayOfMonth(1);
+		DateTime endMonth = endDate.plusMonths(1).withDayOfMonth(1);
 		
 		for (DateTime start = startMonth; start.isBefore(endMonth); start = start.plusDays(nbDaysInPeriod)) {
 			RatingPeriod period = new RatingPeriod(start, start.plusDays(nbDaysInPeriod));
--- trunk/source/thoughtferret.unittests/src/com/thoughtworks/thoughtferret/unittests/RatingAveragesTests.java
+++ trunk/source/thoughtferret.unittests/src/com/thoughtworks/thoughtferret/unittests/RatingAveragesTests.java
@@ -1,25 +1,36 @@
 package com.thoughtworks.thoughtferret.unittests;
 
-import static com.thoughtworks.thoughtferret.unittests.DateBuilder.date;
-import static com.thoughtworks.thoughtferret.unittests.DateBuilder.timestamp;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 import com.thoughtworks.thoughtferret.model.mood.MoodRating;
-import com.thoughtworks.thoughtferret.view.moodgraph.RatingPeriod;
+import com.thoughtworks.thoughtferret.model.mood.MoodRatings;
+import com.thoughtworks.thoughtferret.view.moodgraph.RatingAverages;
 
 public class RatingAveragesTests {
 
 	@Test
 	public void shouldCalculateAveragesWhenAllPeriodsHaveRatings() {
-
+		MoodRatings ratings = new MoodRatings(
+				new MoodRating("04-06-2010 09:45", 3),
+				new MoodRating("22-07-2010 16:28", 3),
+				new MoodRating("17-08-2010 16:03", 1),
+				new MoodRating("09-09-2010 18:16", 2));
+		RatingAverages averages = new RatingAverages(ratings, 30);
+		assertEquals(4, averages.getAverages().size());
 	}
 
 	@Test
 	public void shouldCalculateAveragesWhenAPeriodHasNoRatings() {
-
+		MoodRatings ratings = new MoodRatings(
+				new MoodRating("04-06-2010 09:45", 3),
+				new MoodRating("17-08-2010 16:03", 1),
+				new MoodRating("09-09-2010 18:16", 2));
+		RatingAverages averages = new RatingAverages(ratings, 30);
+		assertEquals(4, averages.getAverages().size());
+		assertFalse(averages.getAverages().get(1).hasRatings());
 	}
 
 	
