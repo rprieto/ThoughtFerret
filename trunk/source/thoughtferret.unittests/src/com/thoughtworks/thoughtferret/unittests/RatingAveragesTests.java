f5f15d1253861f147fc39c254721f6ac251f4ac3
rprieto <rprieto@578f6600-eed7-407d-a15d-0bb1fb5aace9>
2010-08-07 15:20:03 +0000

    Fixed RatingAverage unit test: was expecting the wrong values
    git-svn-id: https://subversion.assembla.com/svn/ThoughtFerret@90 578f6600-eed7-407d-a15d-0bb1fb5aace9

--- trunk/source/thoughtferret.unittests/src/com/thoughtworks/thoughtferret/unittests/RatingAveragesTests.java
+++ trunk/source/thoughtferret.unittests/src/com/thoughtworks/thoughtferret/unittests/RatingAveragesTests.java
@@ -19,7 +19,7 @@ public class RatingAveragesTests {
 				new MoodRating("17-08-2010 16:03", 1),
 				new MoodRating("09-09-2010 18:16", 2));
 		RatingAverages averages = new RatingAverages(ratings, 30);
-		assertEquals(4, averages.getAverages().size());
+		assertEquals(5, averages.getAverages().size());
 	}
 
 	@Test
@@ -29,7 +29,7 @@ public class RatingAveragesTests {
 				new MoodRating("17-08-2010 16:03", 1),
 				new MoodRating("09-09-2010 18:16", 2));
 		RatingAverages averages = new RatingAverages(ratings, 30);
-		assertEquals(4, averages.getAverages().size());
+		assertEquals(5, averages.getAverages().size());
 		assertFalse(averages.getAverages().get(1).hasRatings());
 	}
 
