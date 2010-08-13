f8f8e37b306d4359df69e81c5e3e302ae7277e1d
rprieto <rprieto@578f6600-eed7-407d-a15d-0bb1fb5aace9>
2010-08-13 13:42:10 +0000

    Added simple background to the preferences screen
    git-svn-id: https://subversion.assembla.com/svn/ThoughtFerret@112 578f6600-eed7-407d-a15d-0bb1fb5aace9

--- trunk/source/ThoughtFerret/src/com/thoughtworks/thoughtferret/view/preferences/EditPreferences.java
+++ trunk/source/ThoughtFerret/src/com/thoughtworks/thoughtferret/view/preferences/EditPreferences.java
@@ -1,16 +1,37 @@
 package com.thoughtworks.thoughtferret.view.preferences;
 
-import com.thoughtworks.thoughtferret.R;
-
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.view.View;
+
+import com.thoughtworks.thoughtferret.R;
+import com.thoughtworks.thoughtferret.view.ApplicationBackground;
+import com.thoughtworks.thoughtferret.view.Screen;
 
 public class EditPreferences extends PreferenceActivity {
 
+	Screen screen;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
+		screen = new Screen(this);
+        setBackground();
+	}
+	
+	private void setBackground() {
+		ApplicationBackground appBackground = new ApplicationBackground(this, ApplicationBackground.GradientDirection.HORIZONTAL, true);
+        Bitmap fullBackground = Bitmap.createBitmap(screen.width(), screen.height(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(fullBackground);
+        appBackground.draw(canvas);
+        
+        View content = findViewById(android.R.id.content);
+        BitmapDrawable drawable = new BitmapDrawable(fullBackground); 
+        content.setBackgroundDrawable(drawable);
 	}
 	
 }
