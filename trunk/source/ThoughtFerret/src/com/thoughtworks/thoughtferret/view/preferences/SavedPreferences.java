e4b6afb9918a0a524cde9116435da45b9ca9b93e
rprieto <rprieto@578f6600-eed7-407d-a15d-0bb1fb5aace9>
2010-08-15 09:18:56 +0000

    Forgot to add a file for preferences
    git-svn-id: https://subversion.assembla.com/svn/ThoughtFerret@114 578f6600-eed7-407d-a15d-0bb1fb5aace9

---
+++ trunk/source/ThoughtFerret/src/com/thoughtworks/thoughtferret/view/preferences/SavedPreferences.java
@@ -0,0 +1,8 @@
+package com.thoughtworks.thoughtferret.view.preferences;
+
+public class SavedPreferences {
+
+	public static final String KEY_AGENT_ENABLED = "agentEnabled";
+	public static final String KEY_AGENT_FREQUENCY = "agentFrequency";
+	
+}
