83f2842df01641554c531ad522f9b8937cc5b47c
rprieto <rprieto@578f6600-eed7-407d-a15d-0bb1fb5aace9>
2010-09-07 10:12:26 +0000

    Made map markers 3D using the shadow, and removed old code
    git-svn-id: https://subversion.assembla.com/svn/ThoughtFerret@152 578f6600-eed7-407d-a15d-0bb1fb5aace9

--- trunk/source/thoughtferret/src/com/thoughtworks/thoughtferret/view/map/OfficeOverlay.java
+++ trunk/source/thoughtferret/src/com/thoughtworks/thoughtferret/view/map/OfficeOverlay.java
@@ -1,8 +1,6 @@
 package com.thoughtworks.thoughtferret.view.map;
 
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 
 import android.content.Context;
 import android.graphics.Bitmap;
@@ -26,22 +24,14 @@ public class OfficeOverlay extends Overlay
 	Office office;
 	
 	private Paint nopPaint = new Paint();
-	private Paint markerEdge = new LinePaint(0xFF000000, 2);
+	private Paint markerEdge = new LinePaint(0xFF333333, 1);
+	private Paint shadowedEdge = new LinePaint(0xFF000000, 2);
 	private Paint textPaint = new FontPaint(0xFF000000, 20, Align.CENTER);
-	
-	private List<FillPaint> fills = Arrays.asList(
-			new FillPaint(0XFFFAFAFA),
-			new FillPaint(0XFFFFDDDD),
-			new FillPaint(0XFFFAEADD),
-			new FillPaint(0XFFEEEEDD),
-			new FillPaint(0XFFEAFADD),
-			new FillPaint(0XFFDDFFDD)
-			);
+	private Paint fillPaint = new FillPaint(0XFFFAFAFA);
 	
 	private HashMap<Trend, Bitmap> trendImages;
 
 	private static final int BOX_HEIGHT = 80;
-
 	private static final int BOX_WIDTH = 130;
 	
 	public OfficeOverlay(Context context, Office office) {
@@ -50,20 +40,21 @@ public class OfficeOverlay extends Overlay
 		trendImages.put(Trend.UP, BitmapFactory.decodeResource(context.getResources(), Trend.UP.getResourceId()));
 		trendImages.put(Trend.DOWN, BitmapFactory.decodeResource(context.getResources(), Trend.DOWN.getResourceId()));
 		trendImages.put(Trend.STABLE, BitmapFactory.decodeResource(context.getResources(), Trend.STABLE.getResourceId()));
+		
+        shadowedEdge.setShadowLayer(2f, 1.5f, 2.5f, 0xFF333333);
+        fillPaint.setShadowLayer(2f, 1.5f, 2.5f, 0xFFDDDDDD);
 	}
 	
     @Override
     public void draw(Canvas canvas, MapView mapView, boolean shadow) 
     {
-        super.draw(canvas, mapView, shadow);                   
-
         Point target = new Point();
         mapView.getProjection().toPixels(office.getLocation().getCoordinates().toGeoPoint(), target);
 
         RectF box = new RectF(target.x - BOX_WIDTH / 2, target.y - BOX_HEIGHT / 2, target.x + BOX_WIDTH / 2, target.y + BOX_HEIGHT / 2);
-        
-        Paint background = fills.get(0);
-        canvas.drawRoundRect(box, 15f, 15f, background);
+
+        canvas.drawRoundRect(box, 15f, 15f, shadowedEdge);
+        canvas.drawRoundRect(box, 15f, 15f, fillPaint);
         canvas.drawRoundRect(box, 15f, 15f, markerEdge);
         
         MoodWeather weather = new MoodWeather(office.getAverage());
@@ -73,4 +64,5 @@ public class OfficeOverlay extends Overlay
         canvas.drawBitmap(weatherImage, box.centerX() - 32, box.centerY() - 32, nopPaint);
         canvas.drawBitmap(trendImages.get(office.getTrend()), box.right - 30, box.bottom - 30, nopPaint);
     }
+    
 }
